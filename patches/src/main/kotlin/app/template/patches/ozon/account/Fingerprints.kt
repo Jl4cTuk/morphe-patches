2d75dcdf09e09daf4d619ec8a7201ba20408b1a6
Jl4cTuk <jl4ctuk@gmail.com>
2026-07-21 13:16:23 +0700

    feat: hide Ozon account lottery and review feed

---
+++ patches/src/main/kotlin/app/template/patches/ozon/account/Fingerprints.kt
@@ -0,0 +1,84 @@
+package app.template.patches.ozon.account
+
+import app.morphe.patcher.Fingerprint
+
+object EntryBannerSingleMapperFingerprint : Fingerprint(
+    definingClass =
+        "Lru/ozon/app/android/regulardraw/widgets/entryBannerWidget/v1/core/single/" +
+            "EntryBannerWidgetSingleMapper;",
+    name = "invoke",
+    returnType = "Ljava/util/List;",
+    parameters = listOf(
+        "Lru/ozon/app/android/regulardraw/widgets/entryBannerWidget/v1/data/" +
+            "EntryBannerSubWidget\$Single;",
+        "Ly20/d;",
+    ),
+)
+
+object EntryBannerMultiMapperFingerprint : Fingerprint(
+    definingClass =
+        "Lru/ozon/app/android/regulardraw/widgets/entryBannerWidget/v1/core/multi/" +
+            "EntryBannerWidgetMultiMapper;",
+    name = "invoke",
+    returnType = "Ljava/util/List;",
+    parameters = listOf(
+        "Lru/ozon/app/android/regulardraw/widgets/entryBannerWidget/v1/data/" +
+            "EntryBannerSubWidget\$Multi;",
+        "Ly20/d;",
+    ),
+)
+
+object EntryBannerContentMapperFingerprint : Fingerprint(
+    definingClass =
+        "Lru/ozon/app/android/regulardraw/widgets/entryBannerWidget/v2/core/" +
+            "EntryBannerContentMapper;",
+    name = "invoke",
+    returnType = "Ljava/util/List;",
+    parameters = listOf(
+        "Lru/ozon/app/android/regulardraw/widgets/entryBannerWidget/v2/data/EntryBannerDTO;",
+        "Ly20/d;",
+    ),
+)
+
+object EntryBannerOverlayMapperFingerprint : Fingerprint(
+    definingClass =
+        "Lru/ozon/app/android/regulardraw/widgets/entryBannerWidget/v2/core/" +
+            "EntryBannerOverlayMapper;",
+    name = "invoke",
+    returnType = "Ljava/util/List;",
+    parameters = listOf(
+        "Lru/ozon/app/android/regulardraw/widgets/entryBannerWidget/v2/data/EntryBannerDTO;",
+        "Ly20/d;",
+    ),
+)
+
+object DesignSystemAtomsMapperFingerprint : Fingerprint(
+    definingClass = "Lru/ozon/app/android/widgets/designSystemAtoms/core/DsAtomsMapper;",
+    name = "invoke",
+    returnType = "Ljava/util/List;",
+    parameters = listOf(
+        "Lru/ozon/app/android/widgets/designSystemAtoms/data/DesignSystemAtomsDTO;",
+        "Ly20/d;",
+    ),
+)
+
+object LegacyCellListMapperFingerprint : Fingerprint(
+    definingClass =
+        "Lru/ozon/app/android/widgets/commonTextWidget/cellList/core/CellListV2Mapper;",
+    name = "invoke",
+    returnType = "Ljava/util/List;",
+    parameters = listOf(
+        "Lru/ozon/app/android/widgets/commonTextWidget/cellList/data/CellListV2DTO;",
+        "Ly20/d;",
+    ),
+)
+
+object CellListMapperFingerprint : Fingerprint(
+    definingClass = "Lru/ozon/app/android/common/cellList/v2/core/CellListV2Mapper;",
+    name = "invoke",
+    returnType = "Ljava/util/List;",
+    parameters = listOf(
+        "Lru/ozon/app/android/common/cellList/v2/data/CellListV2DTO;",
+        "Ly20/d;",
+    ),
+)
