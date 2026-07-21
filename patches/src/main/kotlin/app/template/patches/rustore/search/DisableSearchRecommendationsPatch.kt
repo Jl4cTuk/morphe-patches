ca1aab3b1429c9e72488439fcf887502bc77c2c0
Jl4cTuk <jl4ctuk@gmail.com>
2026-07-21 11:31:30 +0700

    feat: add RuStore search cleanup patch

---
+++ patches/src/main/kotlin/app/template/patches/rustore/search/DisableSearchRecommendationsPatch.kt
@@ -0,0 +1,24 @@
+package app.template.patches.rustore.search
+
+import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
+import app.morphe.patcher.patch.bytecodePatch
+import app.template.patches.rustore.shared.Constants.COMPATIBILITY_RUSTORE
+
+@Suppress("unused")
+val disableSearchRecommendationsPatch = bytecodePatch(
+    name = "Disable search recommendations",
+    description = "Removes the Trending and Games sections from search.",
+    default = true,
+) {
+    compatibleWith(COMPATIBILITY_RUSTORE)
+
+    execute {
+        SearchZeroContentConstructorFingerprint.method.addInstructions(
+            0,
+            """
+                const/4 p1, 0x0
+                const/4 p2, 0x0
+            """,
+        )
+    }
+}
